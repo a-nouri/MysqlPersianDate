39393b2c19a9deef88564bf5f44ed98cfcc297b8
abdollah nouri <46811451+a-nouri@users.noreply.github.com>
2021-07-09 13:57:44 +0430

    Create jalali_cur_date.sql

---
+++ jalali_cur_date.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION `get_cur_date`()
+    RETURNS CHAR(8) CHARACTER SET latin1
+    DETERMINISTIC    
+    SQL SECURITY DEFINER
+BEGIN
+  set @y := DATE_FORMAT(CURDATE(),'%Y%m%d');
+  RETURN miltosham2(@y);
+END;
